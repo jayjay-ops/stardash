041e110bd74e196bd6e9f2455e9b77f3fcf3511f
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-10-20 17:51:40 +0200

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2029,7 +2029,7 @@ case 'gif':
 					encmedia = JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo
 					media = await Lxa.downloadAndSaveMediaMessage(encmedia)
 				ran= getRandom('.mp3')
-				exec(`ffmpeg -i ${media} -af  'volume=200'  ${ran} -af 'volume=200' output.mp3  `, (err) => {
+				exec(`ffmpeg -i ${media} -af  'volume=200'  ${ran} `, (err) => {
 						fs.unlinkSync(media)
 						if (err) return reply('Error')
 						buffer = fs.readFileSync(ran)
