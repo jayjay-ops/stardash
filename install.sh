2b341b736a2aed9eef6cb0e4f109f9380e26239b
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-03 15:31:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,14 @@ apt-get install curl
 apt-get install tesseract
 apt-get install mmv
 apt-get install python
-pip install --upgrade youtube_dl
+
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 
 
 
 npm i imgbb-uploader
 npm i
+pip install --upgrade youtube_dl
 node index
 
 echo "[*] Shiba has installed all dependencies, please run the command \"node index\" to start the script"
