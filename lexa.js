56d347ceaddfad3c157250db45bed2b9e0a4357b
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-10 19:24:25 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -1629,7 +1629,7 @@ else if (slot1 == slot2) {
 }
 
 else if (slot2 == slot3) {	
-reply(`★ 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}|${slot2}|${slot3} ☜︎ 𝚂𝚖𝚊𝚕𝚕 𝚠𝚒𝚗 ♕︎\n- - - - - - - - - -\n${slot7}${slot8}${slot9}`)
+reply(`★ 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}|${slot2}|${slot3} ☜︎ 𝚂𝚖𝚊𝚕𝚕 𝚠𝚒𝚗 ♕︎\n- - - - - - - - - \n${slot7}${slot8}${slot9}`)
 
     
 }
