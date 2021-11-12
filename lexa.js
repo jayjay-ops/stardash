bf990b122bc5ac9f9a5a17990231e3d40fb89288
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-12 20:41:02 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2135,6 +2135,12 @@ case 'blackwhite':
 
 case 'testing':
 
+// send a buttons message!
+const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1}
+]
+
 const buttonMessage = {
     contentText: "Hi it's button message",
     footerText: 'Hello World',
