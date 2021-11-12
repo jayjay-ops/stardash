d5d731fe1f4f79078469604437daefbbbffc6a52
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-12 20:42:54 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2135,6 +2135,36 @@ case 'blackwhite':
 
 case 'testing':
 
+const id = 'abcd@s.whatsapp.net' // the WhatsApp ID 
+// send a simple text!
+const sentMsg  = await Lxa.sendMessage (id, 'oh hello there', MessageType.text)
+// send a location!
+const sentMsg  = await Lxa.sendMessage(id, {degreesLatitude: 24.121231, degreesLongitude: 55.1121221}, MessageType.location)
+// send a contact!
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Jeff Singh\n' // full name
+            + 'ORG:Ashoka Uni;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=911234567890:+91 12345 67890\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+const sentMsg  = await Lxa.sendMessage(id, {displayname: "Jeff", vcard: vcard}, MessageType.contact)
+// send a list message!
+const rows = [
+ {title: 'Row 1', description: "Hello it's description 1", rowId:"rowid1"},
+ {title: 'Row 2', description: "Hello it's description 2", rowId:"rowid2"}
+]
+
+const sections = [{title: "Section 1", rows: rows}]
+
+const button = {
+ buttonText: 'Click Me!',
+ description: "Hello it's list message",
+ sections: sections,
+ listType: 1
+}
+
+const sendMsg = await Lxa.sendMessage(id, button, MessageType.listMessage)
+
 // send a buttons message!
 const buttons = [
   {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
