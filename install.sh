98f64b99b7beb3320e829e1595db580fb84f3e7d
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-15 13:55:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,6 @@ apt-get install espeak
 
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 
-
-
 npm i imgbb-uploader
 npm i
 pip install -U yt-dlp
