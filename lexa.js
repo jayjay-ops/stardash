fe683f6b8e5c07edeea30beb4d112447d49c302c
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-15 12:16:15 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -3208,7 +3208,7 @@ case 'register':
 			
 			const userid = Math.floor(Math.random() * 1000000000)
 			
-			fs.appendFile(`${sender.split("@")[0]}@s.whatsapp.net.json`, `["+${sender.split("@")[0]}", "${pushname}", "${args[0]}" , "${args[1]}", "${args[2]}", "${userid}", "0", "100", "-"]`, function (err) {
+			fs.appendFile(`${sender.split("@")[0]}@s.whatsapp.net.json`, `["+${sender.split("@")[0]}", "${pushname}", "${args[0]}" , "${args[1]}", "${args[2]}", "${userid}", "1", "100", "-"]`, function (err) {
 				
 				
 		
