28fdea58a92b08e52b4ae7971d135d63d7446a51
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-15 11:35:48 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -1663,7 +1663,7 @@ const slotme8 = _slotme8[7]	//--- money
   
 else if (slot1 == slot2) {	
 
-let o = Number(11);
+let o = Number(7);
 let op = p + o; 
 fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
     if (err) throw err;
@@ -1687,7 +1687,7 @@ const slotme8 = _slotme8[7]	//--- money
 
 else if (slot2 == slot3) {	
 
-let o = Number(11);
+let o = Number(7);
 let op = p + o; 
 fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
     if (err) throw err;
@@ -1711,7 +1711,7 @@ reply(`★ 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${s
 
 else if (slot1 == slot3) {	
 
-let o = Number(11);
+let o = Number(7);
 let op = p + o; 
 fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
     if (err) throw err;
