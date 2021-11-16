b8861855b1c5abd895a32a07620228fe2a44e2e0
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-16 19:24:28 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2731,9 +2731,9 @@ fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(e
 				
 	
 					})
-const eish = `${sender.split("@")[0]}.webp`		
+
 	
-					exec(`webpmux -set exif ${addMetadata('StarDash', stickerpack1)} ${eish} -o ${eish}`)
+					exec(`webpmux -set exif ${addMetadata('StarDash', stickerpack1)} ${sender.split("@")[0]}.webp -o ${sender.split("@")[0]}.webp`)
 
 const delaydesign1sticker = ms => new Promise(resolve => setTimeout(resolve, ms))
   await delaydesign1sticker(1000) /// waiting 1 second.					
