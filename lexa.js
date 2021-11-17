508c03ce667ad77334cd4280f2174edbf7089ecc
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-17 01:24:49 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -3683,8 +3683,9 @@ if (!isVerify) return reply(userB())
 	
 const _changedata = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s.whatsapp.net.json`));
 
+
 	
-const changedata = args[0] ;   //--- changedata
+const changedata = _changedata args[0];   //--- changedata
 
 
 let newwdata = args[1];
