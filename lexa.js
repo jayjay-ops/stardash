f9ffb042ef05d958cc8e9abd9dcf923b8de34002
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-18 19:46:55 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -4282,7 +4282,7 @@ const _mytransfermoney = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s
 const mytransfermoney = _mytransfermoney[7]	//--- money mine
 
 let arr = args[1].split("@");
-console.log('${arr}');
+console.log(`${arr}`);
 
 const _histranfermoney = JSON.parse(fs.readFileSync(`./${arr}@s.whatsapp.net.json`));	
 const histranfermoney = _histranfermoney[7]	//--- money his
