5804d6de064668148576a4c76980cd730ca1f978
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-19 18:11:33 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -412,10 +412,10 @@ const linkwa = 'http'
 if (isCmd && isBan) return reply(banned())
 
 //-- Automatisch lesen, wenn es eine Nachricht gibt
-if (isCmd && isVerify) Lxa.chatRead(from)
+if (isCmd) Lxa.chatRead(from)
 	
 // -- User   
-if (isCmd)  {
+if (isCmd && isVerify)  {
 	
 const _myaccount = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s.whatsapp.net.json`));	
 	
