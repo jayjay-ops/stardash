816a7a7a53b5c5c2087e8d790b5cdf01f6315cea
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-20 21:08:31 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -2496,13 +2496,13 @@ myMonths = ["January","February","März","April","May","June","Juliy","August","
 			break
 			
 //--- verify after deleted
-case 'password':
+case 'verifyagain':
 
 reply (`What is the password?`)	
 
 	break
 	
-case 'verifyagain':	
+case 'password':	
 
 if (args.length < 1) return reply(`By signing up, you agree to our Terms of Service. To view, send this emojie 🖇️
 . . . . . . . . . . . . . . .
