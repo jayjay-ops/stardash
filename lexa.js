0f56ac4ff07797f48174e8b0ca4f3be48f36164a
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-21 01:14:23 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -1069,7 +1069,7 @@ case 'rlied':
 		
   if (!isVerify) return reply(userB())
   const song = _song[Math.floor(Math.random() * _song.length)]
-  reply(`${rsong} ﹝𝚁𝚊𝚗𝚍𝚘𝚖 𝚜𝚘𝚗𝚐﹞\n. . . . . . . . . . . . . . .\n${song}\n. . . . . . . . . . . . . . .\n✅ StarDash Song`)
+  reply(`${rsongdesign} ﹝𝚁𝚊𝚗𝚍𝚘𝚖 𝚜𝚘𝚗𝚐﹞\n. . . . . . . . . . . . . . .\n${song}\n. . . . . . . . . . . . . . .\n✅ StarDash Song`)
 break
 
 
