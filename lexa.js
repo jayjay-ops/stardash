f1f914c164fceabbc8a481e523bf90092206b4a0
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-27 18:12:51 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -874,6 +874,7 @@ case 'slot':
 		const slot9 = _slot[Math.floor(Math.random() * _slot.length)]
 	
 	
+if 	(slotme7 < 100) {
 	
 				let ps = Number(slotme7);
 
@@ -996,6 +997,133 @@ reply(`${slotdesign} 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - -
 				const slotme8 = _slotme8[7]	//--- money	
 	 
 reply(`${slotdesign} 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}${slot2}${slot3} ☜︎ 𝙽𝚘 𝚠𝚒𝚗 ♕︎\n- - - - - - - - - \n${slot7}${slot8}${slot9}\n\n𝚖𝚘𝚗𝚎𝚢 𝚕𝚎𝚏𝚝 ${slotme8}$`)  
+}
+}
+
+else if 	(slotme7 > 100) {
+	
+					let ps = Number(slotme7);
+
+				if ((slot1 == slot2) && slot2 == slot3) {	
+
+				let os = Number(250);
+				let ops = ps + os; 
+				fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
+					if (err) throw err;
+					
+					var newValue = data.replace(`${slotme7}`, ops);
+					
+					fs.writeFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, newValue, 'utf-8', function(err, data) {
+						if (err) throw err;
+						console.log('Done!');
+					})
+				})
+
+						const delayslot = ms => new Promise(resolve => setTimeout(resolve, ms))
+						await delayslot(3000) /// waiting 1 second.
+
+		const _slotme8 = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s.whatsapp.net.json`));	
+		const slotme8 = _slotme8[7]	//--- money	
+
+  reply(`${slotdesign} 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}${slot2}${slot3} ☜︎ ♕︎ 𝙹𝚊𝚌𝚔𝚙𝚘𝚝 ♕︎\n- - - - - - - - - \n${slot7}${slot8}${slot9}\n\n𝚖𝚘𝚗𝚎𝚢 𝚕𝚎𝚏𝚝 ${slotme8}$`)  
+}  
+  
+else if (slot1 == slot2) {	
+
+			let os = Number(14);
+			let ops = ps + os; 
+			fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
+				if (err) throw err;
+				
+				var newValue = data.replace(`${slotme7}`, ops);
+				
+				fs.writeFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, newValue, 'utf-8', function(err, data) {
+					if (err) throw err;
+					console.log('Done!');
+				})
+			})
+
+					const delayslot = ms => new Promise(resolve => setTimeout(resolve, ms))
+					await delayslot(3000) /// waiting 1 second.
+
+					const _slotme8 = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s.whatsapp.net.json`));	
+					const slotme8 = _slotme8[7]	//--- money	
+
+	reply(`${slotdesign} 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}${slot2}${slot3} ☜︎ 𝚂𝚖𝚊𝚕𝚕 𝚠𝚒𝚗 ♕︎\n- - - - - - - - - \n${slot7}${slot8}${slot9}\n\n𝚖𝚘𝚗𝚎𝚢 𝚕𝚎𝚏𝚝 ${slotme8}$`)  
+}
+
+else if (slot2 == slot3) {	
+
+			let os = Number(14);
+			let ops = ps + os; 
+			fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
+				if (err) throw err;
+				
+				var newValue = data.replace(`${slotme7}`, ops);
+				
+				fs.writeFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, newValue, 'utf-8', function(err, data) {
+					if (err) throw err;
+					console.log('Done!');
+				})
+			})
+
+				const delayslot = ms => new Promise(resolve => setTimeout(resolve, ms))
+                await delayslot(3000) /// waiting 1 second.
+
+				const _slotme8 = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s.whatsapp.net.json`));	
+				const slotme8 = _slotme8[7]	//--- money	
+
+reply(`${slotdesign} 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}${slot2}${slot3} ☜︎ 𝚂𝚖𝚊𝚕𝚕 𝚠𝚒𝚗 ♕︎\n- - - - - - - - - \n${slot7}${slot8}${slot9}\n\n𝚖𝚘𝚗𝚎𝚢 𝚕𝚎𝚏𝚝 ${slotme8}$`)   
+}
+
+else if (slot1 == slot3) {	
+
+				let os = Number(14);
+				let ops = ps + os; 
+				fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
+					if (err) throw err;
+					
+					var newValue = data.replace(`${slotme7}`, ops);
+					
+					fs.writeFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, newValue, 'utf-8', function(err, data) {
+						if (err) throw err;
+						console.log('Done!');
+					})
+				})
+
+					const delayslot = ms => new Promise(resolve => setTimeout(resolve, ms))
+					await delayslot(3000) /// waiting 1 second.
+
+					const _slotme8 = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s.whatsapp.net.json`));	
+					const slotme8 = _slotme8[7]	//--- money	
+
+reply(`${slotdesign} 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}${slot2}${slot3} ☜︎ 𝚂𝚖𝚊𝚕𝚕 𝚠𝚒𝚗 ♕︎\n- - - - - - - - - \n${slot7}${slot8}${slot9}\n\n𝚖𝚘𝚗𝚎𝚢 𝚕𝚎𝚏𝚝 ${slotme8}$`) 
+
+}
+
+ else {
+	
+			let os = Number(4);
+			let ops = ps - os; 
+			fs.readFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, 'utf-8', function(err, data) {
+				if (err) throw err;
+				
+				var newValue = data.replace(`${slotme7}`, ops);
+				
+				fs.writeFile(`./${sender.split("@")[0]}@s.whatsapp.net.json`, newValue, 'utf-8', function(err, data) {
+					if (err) throw err;
+					console.log('Done!');
+				})
+			})
+
+				const delayslot = ms => new Promise(resolve => setTimeout(resolve, ms))
+                await delayslot(3000) /// waiting 1 second.
+
+				const _slotme8 = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s.whatsapp.net.json`));	
+				const slotme8 = _slotme8[7]	//--- money	
+	 
+reply(`${slotdesign} 𝚂𝚕𝚘𝚝\n\n${slot4}${slot5}${slot6}\n- - - - - - - - - \n${slot1}${slot2}${slot3} ☜︎ 𝙽𝚘 𝚠𝚒𝚗 ♕︎\n- - - - - - - - - \n${slot7}${slot8}${slot9}\n\n𝚖𝚘𝚗𝚎𝚢 𝚕𝚎𝚏𝚝 ${slotme8}$`)  
+}
 }
 
 break
