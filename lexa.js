4f98219414f93943e0bb2386f857b12fe3535a4a
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-28 02:51:11 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -4700,7 +4700,9 @@ const hispetfightxp = _hispetfight[4];				//--- xp
 						const attackdely3 = ms => new Promise(resolve => setTimeout(resolve, ms))
 						await attackdely3(1000) /// waiting 1 second.
 
-reply("done")
+reply(`🩸﹝${petfightname} attacked §{hispetfightname}﹞\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\n_§{hispetfightname} lost §{myattack} Lp_\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\n_${petfightname} gained ${petfightstage} Xp_\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\n💟 StarDash Pets`)
+
+
 	break
 
 //-- 
