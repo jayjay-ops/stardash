55175ab3d304d4466f92593cc3f2feff45e3e23b
joewilliams007 <83557178+joewilliams007@users.noreply.github.com>
2021-11-28 17:40:08 +0100

    Update lexa.js

--- lexa.js
+++ lexa.js
@@ -3076,6 +3076,8 @@ const _yourcashpetshop = JSON.parse(fs.readFileSync(`./${sender.split("@")[0]}@s
 const yourcashpetshop = _yourcashpetshop[7]	//--- money	
 const petshopdesign = _yourcashpetshop[11]	//--- account design
 
+if (args[0] == 'elf') return reply ("Please use capital E, .buypet Elf")
+	
 		if (yourcashpetshop < 100) return reply(`﹝You have not enough money to buy a pet. Money needed: 100$ Your money: ${yourcashpetshop}$﹞\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\n💟 StarDash Pets`) 
 		if (args.length < 1) return reply(`${petshopdesign}﹝𝙴𝚗𝚝𝚎𝚛 the Pet. You can choose between Umbreon, Dragon and Elf!﹞\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\nExample: .buypet Elf\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\n💟 StarDash Pets`)
 		if (args.length > 1) return reply(`${petshopdesign}﹝𝙴𝚗𝚝𝚎𝚛 valid Pet. You can choose between Umbreon, Dragon and Elf!﹞\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\nExample: .buypet Elf\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-🍃\n💟 StarDash Pets`)
